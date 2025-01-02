1e38ec937492dd472e586c8a3fa3e06e8685a7f7
smallsilverwest <smallsilverwest@ewha.ac.kr>
2025-01-03 05:37:25 +0900

    gallery to talky 기본 설정 변환

--- app/src/main/java/com/example/talky/ui/talky/TalkyFragment.kt
+++ app/src/main/java/com/example/talky/ui/talky/TalkyFragment.kt
@@ -1,4 +1,4 @@
-package com.example.talky.ui.gallery
+package com.example.talky.ui.talky
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.talky.databinding.FragmentGalleryBinding
+import com.example.talky.databinding.FragmentTalkyBinding
 
-class GalleryFragment : Fragment() {
+class TalkyFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentTalkyBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class GalleryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+        val talkyViewModel =
+            ViewModelProvider(this).get(TalkyViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentTalkyBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
+        talkyViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/com/example/talky/ui/talky/TalkyViewModel.kt
+++ app/src/main/java/com/example/talky/ui/talky/TalkyViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.talky.ui.gallery
+package com.example.talky.ui.talky
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GalleryViewModel : ViewModel() {
+class TalkyViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+        value = "This is talky Fragment"
     }
     val text: LiveData<String> = _text
 }
